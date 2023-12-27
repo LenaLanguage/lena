749ce7d9e8886a75a4fcc782a84e6ac994295e21
MatveyRybalkin <sergey0lga@mail.ru>
2023-12-28 00:32:48 +0600

    Import commit

--- lena/src/lena.h
+++ lena/src/lena.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -23,5 +24,117 @@
 /* Parser (Universal for LI/ENA family languages) */
 #include "parser/parser.h"
 
+
+/* ******************************* FREE ******************************* */
+
+
+void kmp_table(char* sstr, size_t slen, int* table) {
+    uint64_t pos = 2;
+    uint64_t cnd = 0;
+    table[0] = -1;
+    table[1] = 0;
+    while (pos < slen) {
+        if (sstr[pos - 1] == sstr[cnd]) {
+            cnd++;
+            table[pos] = cnd;
+            pos++;
+        } else if (cnd > 0) {
+            cnd = table[cnd];
+        } else {
+            table[pos] = 0;
+            pos++;
+        }
+    }
+}
+
+uint32_t search_in_line(char* str, size_t len, char* sstr, size_t slen) {
+    if (slen == 0) return 0;
+
+    int* table = (int*)malloc(sizeof(int) * slen);
+    if (!table) return UINT32_MAX;
+
+    kmp_table(sstr, slen, table);
+
+    uint64_t m = 0;
+    uint64_t i = 0;
+    while (m + i < len) {
+        if (sstr[i] == str[m + i]) {
+            if (i == slen - 1) {
+                free(table);
+                return m;
+            }
+            i++;
+        } else {
+            m += i - table[i];
+            if (table[i] > -1) {
+                i = table[i];
+            } else {
+                i = 0;
+            }
+        }
+    }
+    free(table);
+    return UINT32_MAX;
+}
+
+#define LENA_SUCCESSFULLY       0
+#define LENA_ITEM_NOT_EXISTS    1
+#define LENA_ERROR              2
+
+uint8_t import_function(char str[], size_t len) {
+    uint32_t pos = search_in_line(str, len, (char *)"import", 6);
+    uint32_t start_name = 0, end_name = 0;
+    if (pos == UINT32_MAX){ return LENA_ITEM_NOT_EXISTS; }
+    pos += 6;
+    uint8_t slash_counter = 0;
+    for(uint32_t i = 0; i < pos; ++i){
+        if(str[i] == '/'){ ++slash_counter; } else { slash_counter = 0; }
+        if(slash_counter == 2){
+            return 1;
+        }
+    }
+    slash_counter = 0;
+    for(uint32_t i = pos; i < len; ++i){
+        if(str[i] != ' ' && str[i] != '\t'){
+            start_name = i;
+            while(str[i] != ' ' && str[i] != '\t' && str[i] != '\n'){
+                if(str[i] == '/'){ ++slash_counter; } else { slash_counter = 0; }
+                if(slash_counter == 2){
+                    /* NAME DOESN'T SET */
+                    return LENA_ERROR;
+                }
+                ++i;
+            }
+            end_name = i;
+            break;
+        }
+    }
+    if (start_name != pos || end_name != pos){
+        for(uint32_t i = start_name; i < end_name; ++i){
+            putc(str[i], stdout);
+        }
+    } else {
+        /* NAME DOESN'T SET */
+        return LENA_ERROR;
+    }
+    return LENA_SUCCESSFULLY;
+}
+
+void analize_code(FILE *src_fptr) {
+    char buffer[SFMLL] = "";
+    bool in_main_function = false;
+    bool in_global_comment = false;
+    uint64_t num_of_line = 0;
+    while(fgets(buffer, SFMLL, src_fptr) != NULL) {
+        ++num_of_line;
+        size_t clen = strlen(buffer);
+        if(import_function(buffer, clen) == LENA_ERROR){
+            printf("[%I64u]: Name doesn't set!", num_of_line);
+        }
+        // printf("%" PRIu64 "\n", search_in_line(buffer, clen, (char *)"console", 3));
+    }
+}
+
+/* ******************************* FREE ******************************* */
 
 #endif // __LENA_I__
--- lena/src/main.c
+++ lena/src/main.c
@@ -1,17 +1,5 @@
 #include "lena.h"
 
-uint32_t search_in_line(char str[], size_t len, char sstr[], size_t slen){
-
-}
-
-void analize_code(FILE *src_fptr) {
-    char buffer[SFMLL] = "";
-    while(fgets(buffer, SFMLL, src_fptr) != NULL) {
-        size_t clen = strlen(buffer);
-        printf("%d\n", (int)clen);
-    }
-}
-
 int main(int argc, char *argv[]){
 
     /* If Lena doesn't have any args */
