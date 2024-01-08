dc4b97b412b7a7952a6e0d3491d7dce56077ab3a
robotsvision <robotsvision.vx@gmail.com>
2024-01-08 15:52:53 +0600

    linux threads

--- lena/src/lthreads/lthreads.h
+++ lena/src/lthreads/lthreads.h
@@ -8,8 +8,12 @@
  * @date     7. Jan 2024
  ******************************************************************************/
 
+/* Standard libraires */
 
-#ifdef _WIN32
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef _WIN32
 
 #include <windows.h>
 
@@ -28,9 +32,24 @@ typedef DWORD lthread_func_t;
 /* lthread create */
 
 lthread_t lthread_create(LPTHREAD_START_ROUTINE func){
-    lthread_t ans = {NULL, 0};
-    ans.hThread = CreateThread(NULL, 0, (func), NULL, 0, &ans.IdThread);
-    return ans;
+    lthread_t thread = {NULL, 0};
+    thread.hThread = CreateThread(NULL, 0, (func), NULL, 0, &thread.IdThread);
+    return thread;
+}
+
+/* lthread get error */
+
+typedef enum {
+    LTHREAD_SUCCESS                 = 0b00000011,
+    /* NEXT LINES OF CODE ARE NOT CROSS-PLATFORM */
+    LTHREAD_ERROR_INVALID_POINTER   = 0b00000001,
+    LTHREAD_ERROR_INVALID_ID        = 0b00000010,
+} lthread_error_t;
+
+lthread_error_t lthread_get_error(lthread_t thread){
+    uint8_t result = 0;
+    result = ((bool)(thread.IdThread) << 1) |((bool)(thread.hThread) << 0);
+    return (lthread_error_t)(result);
 }
 
 /* lthread join */
@@ -46,14 +65,54 @@ void lthread_join(lthread_t* thread, lthread_wait_t wait_ms){
 
 #else
 
+#define _GNU_SOURCE
 #include <pthread.h>
+#include <sys/time.h>
+
+/* lthread basics */
+
+typedef enum {
+    LTHREAD_SUCCESS = 0,
+    /* NEXT LINES OF CODE ARE NOT CROSS-PLATFORM */
+    /** LTHREAD_ERROR_N
+     *  -> N is error code;
+     * 
+    */
+} lthread_error_t;
 
 typedef struct {
     pthread_t pThread;
+    int32_t errThread;
 } lthread_t;
 
-lthread_t lthread_create(void* func){
+typedef void* lthread_func_t;
+#define LENA_API
+#define LTHREAD_ARGS        void* arg
+#define LTHREAD_CALLBACK    NULL
 
+/* lthread create */
+
+lthread_t lthread_create(void* func){
+    lthread_t result = {0, 0};
+    result.errThread = pthread_create(&result.pThread, NULL, func, NULL);
+}
+
+/* lthread get error */
+
+// enum lthread_error_t is defined above
+
+lthread_error_t lthread_get_error(lthread_t thread){
+    return (lthread_error_t)(thread.errThread);
+}
+
+/* lthread join */
+
+#define LTHREAD_WAIT_INFINITE 0
+typedef uint32_t lthread_wait_t;
+
+void lthread_join(lthread_t* thread, lthread_wait_t wait_ms){
+    struct timespec timeout = {0, (wait_ms * 1000)};
+    pthread_timedjoin_np(thread->pThread, NULL, &timeout);
 }
 
 #endif
