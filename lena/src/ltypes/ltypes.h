c2f45b74a20a78927059a33fef5dc65a44d555b0
robotsvision <robotsvision.vx@gmail.com>
2024-01-09 17:41:17 +0600

    types and others

--- lena/src/ltypes/ltypes.h
+++ lena/src/ltypes/ltypes.h
@@ -8,8 +8,21 @@
  * @date     9. Jan 2024
  ******************************************************************************/
 
+/* Standard libraries */
+#include <stdint.h>
+
 /* Libraries */
 #include "lchar.h"
 #include "lstring.h"
+
+typedef uint8_t     lu8_t;
+typedef uint16_t    lu16_t;
+typedef uint32_t    lu32_t;
+typedef uint64_t    lu64_t;
+
+typedef int8_t     l8_t;
+typedef int16_t    l16_t;
+typedef int32_t    l32_t;
+typedef int64_t    l64_t;
 
 #endif // __LENA_TYPES_H__
