ff443486818b39bdd47e608606b7f0188a0cbecc
robotsvision <robotsvision.vx@gmail.com>
2024-02-26 13:12:38 +0600

    lena-i -> makefile correct compilation for llibs .c files

--- lena-i/src/lena-i.c
+++ lena-i/src/lena-i.c
@@ -1,7 +1,10 @@
 #include <lena-i.h>
+#include <stdio.h>
 
 lm lmain(lu32 argc, lc* argv[]) {
-    printf("Hello Lena! Again...");
+    lc* my_str = X"ФФФ";
+    printf("%ld", lslen(my_str));
+    lcout(my_str);
     range(i, 0, argc) {
         printf("%s\n", argv[i]);
     }
