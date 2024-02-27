66f16678de53e6e83bfc91853132a983b29d55ff
robotsvision <robotsvision.vx@gmail.com>
2024-02-27 22:33:24 +0600

    lena-i -> makefile adding lingua framework specific macros

--- lena-i/src/lena-i.c
+++ lena-i/src/lena-i.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 lm lmain(lu32 argc, lc* argv[]) {
-    lc* my_str = X"ФФФ";
-    printf("%ld", lslen(my_str));
-    lcout(my_str);
-    range(i, 0, argc) {
-        printf("%s\n", argv[i]);
-    }
+    lc* my_str = X"你好莉娜!";
+    lcoutn(my_str, 2);
+    
     return 0;
 }
