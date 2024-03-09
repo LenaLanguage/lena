2cd0495f418312961a3a395798bb0d116e866285
robotsvision <robotsvision.vx@gmail.com>
2024-03-09 22:47:31 +0500

    lena-i -> utf-8 final test in Windows

--- lena-i/src/lena-i.c
+++ lena-i/src/lena-i.c
@@ -1,6 +1,10 @@
 #include <lena-i.h>
 
 lm lmain(lu32 argc, lc* argv[]) {
-    llibs_init();
+    if (lcio_init() != L_OK) {
+        lexit(L_EXIT_FAILURE);
+    }
+    lcout(X("世界你好莉娜！"));
+    lcoutn(X("世界你好莉娜！"), 3);
     return 0;
 }
