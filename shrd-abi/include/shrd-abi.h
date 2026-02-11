4a2e426021ee3ce8afed2898c69f77465d619710
kenjaku-rt <kokushibou.rt@gmail.com>
2026-02-11 22:12:37 +0300

    reboot to shared libraries and ABI

---
+++ shrd-abi/include/shrd-abi.h
@@ -0,0 +1,11 @@
+#ifndef SHRD_LENA_ABI_H_
+#define SHRD_LENA_ABI_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+extern void my_function(void);
+
+#endif // SHRD_LENA_ABI_H_
---
+++ shrd-abi/src/shrd-abi.c
@@ -0,0 +1,6 @@
+
+#include <shrd-abi.h>
+
+void my_function(void) {
+    printf("Hello world!");
+}
